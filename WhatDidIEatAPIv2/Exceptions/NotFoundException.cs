f04b6ddb28d655099d357f5af2c0b8075520e337
Ty Bird <t.bird7976@gmail.com>
2025-02-17 10:58:51 -0600

    chore: update formating

--- WhatDidIEatAPIv2/Exceptions/NotFoundException.cs
+++ WhatDidIEatAPIv2/Exceptions/NotFoundException.cs
@@ -2,11 +2,6 @@
 
 public class NotFoundException : Exception
 {
-  public NotFoundException(string? message) : base(message)
-  {
-  }
-
-  public NotFoundException(string? message, Exception innerException) : base(message, innerException)
-  {
-  }
+  public NotFoundException(string? message) : base(message) { }
+  public NotFoundException(string? message, Exception innerException) : base(message, innerException) { }
 }
