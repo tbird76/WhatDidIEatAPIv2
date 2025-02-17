e43c9f119aee46c2ca5075869897ec3745fd1738
Ty Bird <t.bird7976@gmail.com>
2025-02-17 10:59:22 -0600

    feat:L create a response opject to use for meal upsert

---
+++ WhatDidIEatAPIv2/Models/UpsertMealResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WhatDidIEatAPIv2.Models;
+
+public class UpsertMealResponse
+{
+  public string Status { get; set; } = string.Empty;
+  public string? Message { get; set; }
+  public string OrigMealId { get; set; } = string.Empty;
+  public MealDTO Meal { get; set; } = new ();
+}
