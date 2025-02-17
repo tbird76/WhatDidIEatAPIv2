0b1e2f325bbf828b0479882af8895c2ca7de204a
Ty Bird <t.bird7976@gmail.com>
2025-02-17 16:43:30 -0600

    feat: create response for restaurant

---
+++ WhatDidIEatAPIv2/Models/UpsertRestaurantResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WhatDidIEatAPIv2.Models;
+
+public class UpsertRestaurantResponse
+{
+  public string Status { get; set; } = string.Empty;
+  public string? Message { get; set; }
+  public string OrigId { get; set; } = string.Empty;
+  public RestaurantDTO Restaurant { get; set; } = new();
+}
