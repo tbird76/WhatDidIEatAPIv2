743d0a996c0a30642d0e69ffc86496ec548150bb
Ty Bird <t.bird7976@gmail.com>
2025-02-17 10:58:36 -0600

    feat: add tests for meal inserts and updates

--- WhatDidIEatAPIv2.Tests/Data/InMemoryDb.cs
+++ WhatDidIEatAPIv2.Tests/Data/InMemoryDb.cs
@@ -22,8 +22,66 @@ public static class InMemoryDb
             DateCreated = DateTime.Parse("2025-01-01T01:01:01"),
             DateLastUpdated = DateTime.Parse("2025-01-01T01:01:01"),
             Name = "Test Meal 1",
+            Rating = 1,
+            Comment = "That was awful",
+            PictureName = "Test.jpg"
+          },
+          new Meal
+          {
+            Id = 2,
+            DateCreated = DateTime.Parse("2025-02-02T02:02:02"),
+            DateLastUpdated = DateTime.Parse("2025-02-02T02:02:02"),
+            Name = "Test Meal 2",
+            Rating = 2,
+            Comment = "Wouldn't eat again",
+            PictureName = "Test.jpg"
+          },
+          new Meal
+          {
+            Id = 3,
+            DateCreated = DateTime.Parse("2025-03-03T03:03:03"),
+            DateLastUpdated = DateTime.Parse("2025-03-03T03:03:03"),
+            Name = "Test Meal 3",
+            Rating = 3,
+            Comment = "It was just ok",
+            PictureName = "Test.jpg"
+          },
+          new Meal
+          {
+            Id = 4,
+            DateCreated = DateTime.Parse("2025-04-04T04:04:04"),
+            DateLastUpdated = DateTime.Parse("2025-04-04T04:04:04"),
+            Name = "Test Meal 4",
+            Rating = 4,
+            Comment = "I would order again",
+            PictureName = "Test.jpg"
+          },
+          new Meal
+          {
+            Id = 5,
+            DateCreated = DateTime.Parse("2025-05-05T05:05:05"),
+            DateLastUpdated = DateTime.Parse("2025-05-05T05:05:05"),
+            Name = "Test Meal 5",
             Rating = 5,
-            Comment = "Wow what an amazing test",
+            Comment = "I'm getting this every time",
+            PictureName = "Test.jpg"
+          }
+        }
+      },
+      new Restaurant
+      {
+        Id = 2,
+        Name = "Restaurant 2",
+        Meals = new List<Meal>
+        {
+          new Meal
+          {
+            Id = 6,
+            DateCreated = DateTime.Parse("2025-06-06T06:06:06"),
+            DateLastUpdated = DateTime.Parse("2025-06-06T06:06:06"),
+            Name = "Test Meal 6",
+            Rating = 6,
+            Comment = "I'm getting this every time",
             PictureName = "Test.jpg"
           }
         }
--- WhatDidIEatAPIv2.Tests/Services/MealServiceTests.cs
+++ WhatDidIEatAPIv2.Tests/Services/MealServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿using Moq;
 using WhatDidIEatAPIv2.Data;
+using WhatDidIEatAPIv2.Exceptions;
 using WhatDidIEatAPIv2.ModelFactories;
+using WhatDidIEatAPIv2.Models;
 using WhatDidIEatAPIv2.Services;
 
 namespace WhatDidIEatAPIv2.Tests.Services;
@@ -25,11 +27,83 @@ public class MealServiceTests : BaseTest
   [Test]
   public async Task GetMeals_MealsAvailable_Success()
   {
+    var meals = await _mealService.GetMealsAsync(false);
+    Assert.That(meals.Count(), Is.EqualTo(6));
   }
 
   [Test]
-  public void GetMeals_MealsUnavailable_Exception()
+  public async Task GetMealById_MealAvailable_Success()
   {
+    var meal = await _mealService.GetMealByIdAsync(1, false);
+    Assert.That(meal is not null);
+  }
 
+  [Test]
+  public void GetMealById_MealUnavailable_Exception()
+  {
+    Assert.ThrowsAsync<NotFoundException>(async () => await _mealService.GetMealByIdAsync(100, false));
+  }
+
+  [Test]
+  public async Task GetMealsByRestaurantId_MealsAvailable_Success()
+  {
+    var meals = await _mealService.GetMealsByRestaurantIdAsync(1, false);
+    Assert.That(meals.Count(), Is.EqualTo(5));
+  }
+
+  [Test]
+  public void GetMealsByRestaurantId_MealsUnavailable_Exception()
+  {
+    Assert.ThrowsAsync<NotFoundException>(async () => await _mealService.GetMealsByRestaurantIdAsync(100, false));
+  }
+
+  [Test]
+  public async Task UpsertMeal_NewMeal_201Status()
+  {
+    var meal = new MealDTO()
+    {
+      Id = Guid.NewGuid().ToString(),
+      DateCreated = DateTime.Parse("2025-06-06T06:06:06"),
+      DateLastUpdated = DateTime.Parse("2025-06-06T06:06:06"),
+      Name = "Insert Test",
+      Rating = 100,
+      Comment = "Wow what an amazing test",
+      PictureName = "Test.jpg"
+    };
+
+    var response = await _mealService.UpsertMealAsync(meal);
+    Assert.That(response.Status, Is.EqualTo("201"));
+    Assert.That(response.Meal.Id , Is.EqualTo("7"));
+    Assert.That(response.Meal.Id, Is.Not.EqualTo(meal.Id));
+  }
+
+  [Test]
+  public async Task UpsertMeal_ExistingMeal_200Status()
+  {
+    var meal = new MealDTO()
+    {
+      Id = Convert.ToString(5),
+      DateCreated = DateTime.Parse("2025-05-05T05:05:05"),
+      DateLastUpdated = DateTime.Parse("2025-05-05T05:05:05"),
+      Name = "Update Test",
+      Rating = 3,
+      Comment = "I'm getting this every time",
+      PictureName = "Test.jpg"
+    };
+
+    var response = await _mealService.UpsertMealAsync(meal);
+    Assert.That(response.Status, Is.EqualTo("200"));
+    Assert.That(response.Meal.Id, Is.EqualTo("5"));
+    Assert.That(response.Meal.Rating, Is.EqualTo(3));
+    Assert.That(response.Meal.Name, Is.EqualTo("Update Test"));
+  }
+
+  [Test]
+  public async Task UpsertMeal_NullMeal_400Status()
+  {
+    MealDTO? meal = null;
+
+    var response = await _mealService.UpsertMealAsync(meal);
+    Assert.That(response.Status, Is.EqualTo("400"));
   }
 }
